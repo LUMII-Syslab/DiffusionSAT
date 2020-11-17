90bf4f8752998f8f5bcde720f992b318e9aabe21
Emīls Ozoliņš <ozolinsemils@gmail.com>
2020-11-17 10:36:32 +0200

    Rewrite config with PyCharm support

--- config.py
+++ config.py
@@ -2,22 +2,58 @@ import argparse
 
 from registry.registry import ModelRegistry, DatasetRegistry
 
-config = argparse.ArgumentParser()
 
-config.add_argument('--train_dir', type=str, default='/host-dir/np-solver')
-config.add_argument('--data_dir', type=str, default='/host-dir/data')
-config.add_argument('--restore', type=str, default=None)
-config.add_argument('--label', type=str, default="")
+class Config:
+    train_dir = '/host-dir/np-solver'
+    data_dir = '/host-dir/data'
+    restore = None
+    label = ""
 
-config.add_argument('--ckpt_count', type=int, default=3)
-config.add_argument('--eager', action='store_true', default=False)
+    ckpt_count = 3
+    eager = False
 
-config.add_argument('--optimizer', default='radam', const='radam', nargs='?', choices=["radam"])
-config.add_argument('--train_steps', type=int, default=300000)
-config.add_argument('--warmup', type=float, default=0.0)
-config.add_argument('--learning_rate', type=float, default=0.0001)
+    optimizer = 'radam'
+    train_steps = 300000
+    warmup = 0.0
+    learning_rate = 0.0001
+    model = 'matrix_se'
+    task = 'euclidean_tsp'
 
-config.add_argument('--model', type=str, default='matrix_se', const='matrix_se', nargs='?',
-                    choices=ModelRegistry().registered_names)
-config.add_argument('--task', type=str, default='euclidean_tsp', const='euclidean_tsp', nargs='?',
-                    choices=DatasetRegistry().registered_names)
+    __arguments_parsed = False
+
+    @classmethod
+    def parse_config(cls):
+        if cls.__arguments_parsed:
+            raise RuntimeError("Arguments already parsed!")
+
+        config = cls.__argument_parser().parse_args()
+        for key, value in config.__dict__.items():
+            setattr(cls, key, value)
+        cls.__arguments_parsed = True
+
+    @classmethod
+    def __argument_parser(cls):
+        config_parser = argparse.ArgumentParser()
+
+        config_parser.add_argument('--train_dir', type=str, default=cls.train_dir)
+        config_parser.add_argument('--data_dir', type=str, default=cls.data_dir)
+        config_parser.add_argument('--restore', type=str, default=None)
+        config_parser.add_argument('--label', type=str, default=cls.label)
+
+        config_parser.add_argument('--ckpt_count', type=int, default=cls.ckpt_count)
+        config_parser.add_argument('--eager', action='store_true', default=cls.eager)
+
+        config_parser.add_argument('--optimizer', default=cls.optimizer, const=cls.optimizer, nargs='?',
+                                   choices=["radam"])
+
+        config_parser.add_argument('--train_steps', type=int, default=cls.train_steps)
+        config_parser.add_argument('--warmup', type=float, default=cls.warmup)
+        config_parser.add_argument('--learning_rate', type=float, default=cls.warmup)
+
+        config_parser.add_argument('--model', type=str, default=cls.model, const=cls.model, nargs='?',
+                                   choices=ModelRegistry().registered_names)
+
+        config_parser.add_argument('--task', type=str, default=cls.task, const=cls.task, nargs='?',
+                                   choices=DatasetRegistry().registered_names)
+
+        return config_parser
