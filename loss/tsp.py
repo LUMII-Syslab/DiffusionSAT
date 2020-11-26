52c6f9bc362448b1740f0c721007e740d07b7567
AndisDraguns <andis.draguns@gmail.com>
2020-11-26 13:18:10 +0000

    Adding TSP metrics (greedy, beam or random search on input or output of the model)

--- loss/tsp.py
+++ loss/tsp.py
@@ -3,14 +3,14 @@ from loss.supervised_tsp import tsp_supervised_loss
 
 def tsp_loss(predictions, adjacency_matrix, coords, noise=0):
     # hybrid loss
-    supervised_loss = tsp_supervised_loss(predictions, adjacency_matrix, coords)
-    unsupervised_loss = tsp_unsupervised_loss(predictions, adjacency_matrix, noise)
-    return 1.0*supervised_loss + 1.0*unsupervised_loss
+    # supervised_loss = tsp_supervised_loss(predictions, adjacency_matrix, coords)
+    # unsupervised_loss = tsp_unsupervised_loss(predictions, adjacency_matrix, noise)
+    # return 1.0*supervised_loss + 1.0*unsupervised_loss
 
     # just unsupervised loss (faster than hybrid loss):
     # unsupervised_loss = tsp_unsupervised_loss(predictions, adjacency_matrix, noise)
     # return unsupervised_loss
-    #
+
     # just unsupervised loss (faster than hybrid loss):
-    # supervised_loss = tsp_supervised_loss(predictions, adjacency_matrix, coords)
-    # return supervised_loss
+    supervised_loss = tsp_supervised_loss(predictions, adjacency_matrix, coords)
+    return supervised_loss
