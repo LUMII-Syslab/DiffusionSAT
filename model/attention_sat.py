f88e42da5980b78f69f37b3d68eee6c420eb05ae
Emīls Ozoliņš <ozolinsemils@gmail.com>
2020-12-02 09:39:17 +0200

    MLP attention
    Use simple MLP and sigmoid to predict edge weights

--- model/attention_sat.py
+++ model/attention_sat.py
@@ -2,16 +2,15 @@ import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Optimizer
 
-from layers.attention import GraphAttentionLayer
+from layers.attention import MLPAttention
 from layers.layer_normalization import LayerNormalization
 from loss.sat import softplus_log_square_loss, unsat_clause_count
 from model.mlp import MLP
-import tensorflow_addons as tfa
 
 
 class AttentionSAT(Model):
 
-    def __init__(self, optimizer: Optimizer, feature_maps=256, msg_layers=3, vote_layers=3, rounds=16, **kwargs):
+    def __init__(self, optimizer: Optimizer, feature_maps=256, msg_layers=3, vote_layers=3, rounds=32, **kwargs):
         super().__init__(**kwargs, name="AttentionSAT")
         self.rounds = rounds
         self.optimizer = optimizer
@@ -23,8 +22,12 @@ class AttentionSAT(Model):
         self.literals_mlp = MLP(msg_layers, feature_maps, feature_maps, do_layer_norm=False)
         self.clauses_mlp = MLP(msg_layers, feature_maps, feature_maps, do_layer_norm=False)
 
-        self.attention_l = GraphAttentionLayer(feature_maps, feature_maps, name="attention_l")
-        self.attention_c = GraphAttentionLayer(feature_maps, feature_maps, name="attention_c")
+        self.attention_l = MLPAttention(feature_maps, feature_maps, name="attention_l")
+        self.attention_c = MLPAttention(feature_maps, feature_maps, name="attention_c")
+
+        self.layer_norm_1 = LayerNormalization(axis=-1)
+        self.layer_norm_2 = LayerNormalization(axis=-1)
+
         self.layer_norm_3 = LayerNormalization(axis=-1)
         self.layer_norm_4 = LayerNormalization(axis=-1)
 
@@ -51,14 +54,14 @@ class AttentionSAT(Model):
 
         for step in tf.range(self.rounds):
             new_clauses = self.attention_c(c_output, l_output, tf.sparse.transpose(adj_matrix))
-            c_output = tf.concat([c_output, new_clauses], axis=-1)
+            c_output = self.layer_norm_1(c_output + new_clauses, training=training)
             new_clauses2 = self.clauses_mlp(c_output, training=training)
-            c_output = self.layer_norm_4(new_clauses2)
+            c_output = self.layer_norm_4(c_output + new_clauses2)
 
             new_literals = self.attention_l(l_output, c_output, adj_matrix)
-            l_output = tf.concat([l_output, self.flip(new_literals, n_vars)], axis=-1)
+            l_output = self.layer_norm_2(l_output + self.flip(new_literals, n_vars), training=training)
             new_literals2 = self.literals_mlp(l_output, training=training)
-            l_output = self.layer_norm_3(new_literals2)
+            l_output = self.layer_norm_3(l_output + new_literals2)
 
             variables = tf.concat([l_output[:n_vars], l_output[n_vars:]], axis=1)  # n_vars x 2
             logits = self.output_layer(variables)
@@ -70,6 +73,9 @@ class AttentionSAT(Model):
             n_unsat_clauses = unsat_clause_count(logits, clauses)
             if loss < 0.5 and n_unsat_clauses == 0:
                 break
+
+            l_output = tf.stop_gradient(l_output) * 0.2 + l_output * 0.8
+            c_output = tf.stop_gradient(c_output) * 0.2 + c_output * 0.8
 
         tf.summary.scalar("steps_taken", step)
         return logits, tf.reduce_mean(step_loss.stack())
