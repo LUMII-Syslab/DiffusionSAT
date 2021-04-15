798ca5a371f16c8b01fe82300afcbb78cabb807b
Emīls Ozoliņš <ozolinsemils@gmail.com>
2021-04-15 17:32:06 +0300

    Cactus solver, times should be sorted by hardness (variable count)

--- evaluate_solvers.py
+++ evaluate_solvers.py
@@ -16,8 +16,9 @@ def test_walksat():
                                                      input_mode=Config.input_mode)
 
     solved = []
+    var_count = []
     time_used = []
-    for step_data in itertools.islice(dataset.test_data(), 10):
+    for step_data in itertools.islice(dataset.test_data(), 125):
         clauses = [x.numpy() for x in step_data["normal_clauses"]]
         vars_in_graph = step_data["variables_in_graph"].numpy()
 
@@ -25,11 +26,13 @@ def test_walksat():
             dimacs = build_dimacs_file(iclauses, n_vars)
             sat, solution, time_elapsed = walksat(dimacs)
             solved.append(int(sat))
+            var_count.append(n_vars)
             time_used.append(time_elapsed)
 
-    cum_solved = numpy.cumsum(solved)
-    cum_time = numpy.cumsum(time_used)
-    rows = [[sol, tim] for sol, tim in zip(cum_solved, cum_time)]
+    rows = [[x, y, z] for x, y, z in zip(var_count, solved, time_used)]
+    rows = sorted(rows, key=lambda x: x[0])
+    rows = [[y, z] for _, y, z in rows]
+    rows = numpy.cumsum(rows, axis=0)
 
     with open("walksat_cactus.csv", "w", newline='') as file:
         writer = csv.writer(file)
