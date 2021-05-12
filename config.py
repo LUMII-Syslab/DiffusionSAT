9e51ab5aff937242c0522ddb18420f3e061472c1
Emīls Ozoliņš <ozolinsemils@gmail.com>
2021-05-12 09:15:50 +0300

    Ablation: QuerySAT without noise on 3-SAT

--- config.py
+++ config.py
@@ -18,11 +18,11 @@ class Config:
 
     """Training and task selection config: """
     optimizer = 'radam'
-    train_steps = 500000
+    train_steps = 100000
     warmup = 0.0
     learning_rate = 0.0002
-    model = 'neurocore'  # query_sat,  query_sat_lit, neuro_sat, tsp_matrix_se
-    task = 'k_color'  # k_sat, k_color, 3-sat, clique, primes, sha-gen2019, dominating_set, euclidean_tsp, asymmetric_tsp
+    model = 'query_sat'  # query_sat,  query_sat_lit, neuro_sat, tsp_matrix_se
+    task = '3-sat'  # k_sat, k_color, 3-sat, clique, primes, sha-gen2019, dominating_set, euclidean_tsp, asymmetric_tsp
     input_mode = 'literals'  # "variables" or "literals", applicable to SAT
 
     """Supported training and evaluation modes: """
