e2fb913281c0230039407506e0c098733f93ec06
Emīls Ozoliņš <ozolinsemils@gmail.com>
2021-05-14 16:35:10 +0300

    SHA-1: NeuroCore second run

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config:
     train_steps = 1000000
     warmup = 0.0
     learning_rate = 0.0002
-    model = 'query_sat'  # query_sat,  query_sat_lit, neuro_sat, tsp_matrix_se
+    model = 'neurocore'  # query_sat,  query_sat_lit, neuro_sat, tsp_matrix_se
     task = 'sha-gen2019'  # k_sat, k_color, 3-sat, clique, primes, sha-gen2019, dominating_set, euclidean_tsp, asymmetric_tsp
     input_mode = 'literals'  # "variables" or "literals", applicable to SAT
 
