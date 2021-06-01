85b17ff90fa843b8d1c116ed7ada9c5ea4031fa6
Emīls Ozoliņš <ozolinsemils@gmail.com>
2021-06-01 11:14:52 +0300

    Remove LayerNormalization

--- layers/normalization.py
+++ layers/normalization.py
@@ -1,33 +1,11 @@
 import tensorflow as tf
 
 
-class LayerNormalization(tf.keras.layers.Layer):
-
-    def __init__(self, axis=1, epsilon=1e-6, subtract_mean=False, **kwargs):
-        self.axis = axis
-        self.epsilon = epsilon
-        self.bias = None
-        self.subtract_mean = subtract_mean
-        super(LayerNormalization, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        if self.subtract_mean:
-            num_units = input_shape.as_list()[-1]
-            self.bias = self.add_weight("bias", [num_units], initializer=tf.zeros_initializer)
-
-    def call(self, inputs, **kwargs):
-        if self.subtract_mean:  # subtracting mean may not be necessary: https://arxiv.org/abs/1910.07467
-            inputs -= tf.reduce_mean(inputs, axis=self.axis, keepdims=True)
-            inputs += self.bias
-
-        variance = tf.reduce_mean(tf.square(inputs), self.axis, keepdims=True)
-        return inputs * tf.math.rsqrt(variance + self.epsilon)
-
-
 class PairNorm(tf.keras.layers.Layer):
     """
     PairNorm: Tackling Oversmoothing in GNNs https://arxiv.org/abs/1909.12223
     """
+
     def __init__(self, epsilon=1e-6, subtract_mean=False, **kwargs):
         self.epsilon = epsilon
         self.bias = None
